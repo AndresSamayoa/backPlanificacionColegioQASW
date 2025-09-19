58db1852c78258d803e787cfe6faaecc2920eda7
asamayoa <andressamayoap@gmail.com>
2025-09-18 18:40:51 -0600

    fix(order) of search apis

--- routes/v1.js
+++ routes/v1.js
@@ -13,8 +13,8 @@ app.delete('/logout', authController.logout);
 app.post('/users', auth, usuariosController.create);
 app.put('/users/:id', auth, usuariosController.update);
 app.delete('/users/:id', auth, usuariosController.delete);
-app.get('/users/:id', auth, usuariosController.getOne);
 app.get('/users/search/:param', auth, usuariosController.search);
+app.get('/users/:id', auth, usuariosController.getOne);
 
 //Azzu
 const recursoController = require('../controllers/v1/recursoController');
@@ -31,21 +31,21 @@ const planiController = require('../controllers/v1/planiController');
 app.post('/recurso',auth, recursoController.create);
 app.put('/recurso/:id',auth,  recursoController.update);
 app.delete('/recurso/:id',auth,  recursoController.delete);
-app.get('/recurso/:id',auth,  recursoController.getOne);
 app.get('/recurso/search/:param',auth,  recursoController.search);
+app.get('/recurso/:id',auth,  recursoController.getOne);
 
 //verificar que todo este bien
 app.post('/grado',auth, gradoController.create);
 app.put('/grado/:id', auth, gradoController.update);
 app.delete('/grado/:id',auth,  gradoController.delete);
-app.get('/grado/:id',auth,  gradoController.getOne);
 app.get('/grado/search/:param',auth,  gradoController.search);
+app.get('/grado/:id',auth,  gradoController.getOne);
 
 app.post('/curso',auth, cursoController.create);
 app.put('/curso/:id', auth, cursoController.update);
 app.delete('/curso/:id',auth,  cursoController.delete);
-app.get('/curso/:id',auth,  cursoController.getOne);
 app.get('/curso/search/:param',auth,  cursoController.search);
+app.get('/curso/:id',auth,  cursoController.getOne);
 
 app.post('/bloque',auth, bloqueController.create);
 app.put('/bloque/:id', auth, bloqueController.update);
@@ -56,27 +56,27 @@ app.get('/bloque/:id',auth,  bloqueController.getOne);
 app.post('/evaluacion',auth, evaluacionController.create);
 app.put('/evaluacion/:id',auth,  evaluacionController.update);
 app.delete('/evaluacion/:id',auth,  evaluacionController.delete);
-app.get('/evaluacion/:id',auth,  evaluacionController.getOne);
 app.get('/evaluacion/search/:param',auth,  evaluacionController.search);
+app.get('/evaluacion/:id',auth,  evaluacionController.getOne);
 
 app.post('/rol',auth, rolController.create);
 app.put('/rol/:id',auth,  rolController.update);
 app.delete('/rol/:id', auth, rolController.delete);
 app.get('/rol/:id', auth, rolController.getOne);
-app.get('/rol/:id',auth,  rolController.getOne1);
 app.get('/rol/search/:param', auth, rolController.search);
+app.get('/rol/:id',auth,  rolController.getOne1);
 
 app.post('/horario',auth, horarioController.create);
 app.put('/horario/:id', auth, horarioController.update);
 app.delete('/horario/:id',auth,  horarioController.delete);
-app.get('/horario/:id',auth,  horarioController.getOne);
 app.get('/horario/search/:param',auth,  horarioController.search);
+app.get('/horario/:id',auth,  horarioController.getOne);
 
 app.post('/asignacion',auth, asignacionController.create);
 app.put('/asignacion/:id',auth,  asignacionController.update);
 app.delete('/asignacion/:id', auth, asignacionController.delete);
-app.get('/asignacion/:id', auth, asignacionController.getOne);
 app.get('/asignacion/search/:param',auth,  asignacionController.search);
+app.get('/asignacion/:id', auth, asignacionController.getOne);
 
 
 app.post('/planificacion', auth, planiController.create);
